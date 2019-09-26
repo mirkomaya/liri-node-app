f77463dd44e723ac60cc9ccf4fd9252102a8b07c
Marty McFly <mirko.maya@live.com>
2019-09-25 23:40:22 -0400

    Working on Spotify response.

--- liri.js
+++ liri.js
@@ -44,55 +44,17 @@ Date of Event:  ${moment.utc(bandsInfo.datetime).format("MM/DD/YYYY")}
 
 function spotifyThis() {
 
-    // var nodeArgs = process.argv;
-
-    // var songName = "";
-
-    // for (var i = 3; i < nodeArgs.length; i++) {
-
-    //     if (i > 3 && i < nodeArgs.length) {
-    //         songName = songName + "+" + nodeArgs[i];
-    //     } else {
-    //         songName += nodeArgs[i];
-    //     }
-    // }
-
-    // console.log(songName)
-
     var songName = process.argv.slice(3).join(" ");
-
-    // spotify
-    //     .search({ type: 'track', query: songName })
-    //     .then(function (response) {
-    //         console.log(response);
-    //     })
-    //     .catch(function (err) {
-    //         console.log(err);
-    //     });
 
     spotify
         .search({ type: 'track', query: songName })
         .then(function (response) {
-            console.log(response.tracks.items);
+            console.log(JSON.stringify(response.tracks, null, 2));
         })
         .catch(function (err) {
             console.log(err);
         });
-
-
-    // spotify
-    //     .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-    //     .then(function (data) {
-    //         console.log(data);
-    //     })
-    //     .catch(function (err) {
-    //         console.error('Error occurred: ' + err);
-    //     });
 }
-
-
-
-//06a0bf01f7da2f97b38deff3c911b9cd
 
 var liriCommand = process.argv[2];
 
